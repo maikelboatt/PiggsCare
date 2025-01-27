bcab90a8dd4941d9b970ad95b1e53e2dc32b1040
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-01-27 09:09:53 +0000

    Refactored the Animal class to use Primary Constructors and removed the required access modifier

--- PiggsCare.Domain/Models/Animal.cs
+++ PiggsCare.Domain/Models/Animal.cs
@@ -1,16 +1,26 @@
 namespace PiggsCare.Domain.Models
 {
-    public class Animal
+    public class Animal(
+        int animalId,
+        int name,
+        string breed,
+        DateTime birthDate,
+        int certificateNumber,
+        Gender gender,
+        float backFatIndex )
     {
-        public int AnimalId { get; init; }
-        public required int Name { get; init; }
-        public required string Breed { get; init; }
-        public required DateTime BirthDate { get; init; }
-        public required int CertificateNumber { get; init; }
+        public int AnimalId { get; init; } = animalId;
+        public int Name { get; init; } = name;
+        public string Breed { get; init; } = breed;
+        public DateTime BirthDate { get; init; } = birthDate;
+        public int CertificateNumber { get; init; } = certificateNumber;
+        public Gender Gender { get; init; } = gender;
+        public float BackFatIndex { get; init; } = backFatIndex;
 
         public override string ToString()
         {
-            return $"Animal Id: {AnimalId}, Animal Name: {Name}, Breed: {Breed}, BirthDate: {BirthDate}, CertificateNumber: {CertificateNumber}";
+            return
+                $"Animal Id: {AnimalId}, Animal Name: {Name}, Breed: {Breed}, BirthDate: {BirthDate}, CertificateNumber: {CertificateNumber}, Gender: {Gender}, BackFatIndex: {BackFatIndex}";
         }
     }
 }

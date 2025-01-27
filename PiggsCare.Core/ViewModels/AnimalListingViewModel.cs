4120efbcb051615f19e1daf8f8038f0de830df60
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-01-27 09:16:33 +0000

    Refactored the AnimalListing view and viewmodel for better UX

--- PiggsCare.Core/ViewModels/AnimalListingViewModel.cs
+++ PiggsCare.Core/ViewModels/AnimalListingViewModel.cs
@@ -1,57 +1,117 @@
 using MvvmCross.Commands;
-using MvvmCross.Plugin.Messenger;
 using MvvmCross.ViewModels;
+using PiggsCare.Core.Control;
 using PiggsCare.Domain.Models;
 using PiggsCare.Domain.Services;
+using System.Collections.Specialized;
 
 namespace PiggsCare.Core.ViewModels
 {
     public class AnimalListingViewModel:MvxViewModel
     {
-        private static IEnumerable<Animal> animalsEnumerable = [];
-        private readonly MvxObservableCollection<Animal> _animals = new(animalsEnumerable);
+        #region Constructor
 
-        private readonly IAnimalService _animalService;
-        // private readonly MvxSubscriptionToken _token;
-
-        public AnimalListingViewModel( IAnimalService animalService, IMvxMessenger messenger )
+        public AnimalListingViewModel( IAnimalService animalService, IModalNavigationControl modalNavigationControl )
         {
             _animalService = animalService;
-            // _token = messenger.Subscribe<AnimalMessage>(AnimalListMessage);
+            _modalNavigationControl = modalNavigationControl;
+            _animals.CollectionChanged += AnimalsOnCollectionChanged;
         }
 
-        public IEnumerable<Animal> Animals => _animals;
+        #endregion
 
-        public IMvxAsyncCommand LoadAnimalsCommand => new MvxAsyncCommand(LoadAnimalsAsyncExecute);
+        #region Fields
 
-        // private void AnimalListMessage( AnimalMessage animals )
-        // {
-        //     animalsEnumerable = animals.Animals;
-        // }
+        private readonly MvxObservableCollection<Animal> _animals = [];
+        private readonly IAnimalService _animalService;
+
+        // private static readonly IEnumerable<Animal> _animalsEnumerable = [];
+        private readonly IModalNavigationControl _modalNavigationControl;
+        private bool _isLoading;
+
+        #endregion
+
+        #region ViewModelLifeCycle
 
         public override void Prepare()
         {
+            Console.WriteLine("Prepare animals listing");
             base.Prepare();
         }
 
         public override async Task Initialize()
         {
-            await LoadAnimalsAsyncExecute();
-
+            await LoadAnimalsAsync();
             await base.Initialize();
         }
 
-        private async Task LoadAnimalsAsyncExecute()
+        #endregion
+
+        #region Properties
+
+        public bool IsLoading
         {
+            get => _isLoading;
+            set => SetProperty(ref _isLoading, value);
+        }
+
+        public IEnumerable<Animal> Animals => _animals;
+
+        public IMvxAsyncCommand LoadAnimalsCommand => new MvxAsyncCommand(TestCrudOperations);
+
+        #endregion
+
+        #region Methods
+
+        private void AnimalsOnCollectionChanged( object? sender, NotifyCollectionChangedEventArgs e )
+        {
+            RaisePropertyChanged(nameof(Animals));
+        }
+
+
+        private async Task LoadAnimals()
+        {
+            _animals.Clear();
+            IEnumerable<Animal> animals = await _animalService.GetAllAnimalsAsync();
+            foreach (Animal animal in animals)
+            {
+                _animals.Add(animal);
+            }
+        }
+
+        private async Task LoadAnimalsAsync()
+        {
+            IsLoading = true;
             try
             {
-                animalsEnumerable = await _animalService.GetAllAnimalsAsync();
+                await LoadAnimals();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
             }
+            finally
+            {
+                IsLoading = false;
+            }
         }
+
+        private async Task TestCrudOperations()
+        {
+            // const Gender male = Gender.Male;
+            // string? maleString = Enum.GetName(male);
+            // Animal record = new(102, "Oestrus", DateTime.Now, 33, maleString, 34.16F);
+            // await _animalService.CreateAnimalAsync(record);
+
+            // Animal? animal = await _animalService.GetAnimalByNameAsync(102);
+            //
+            // // foreach (Animal animal in animals)
+            // Console.WriteLine(animal);
+            // _modalNavigationControl.PopUp<TestViewModel>(1);
+            // _modalNavigationControl.Open<TestViewModel>();
+        }
+
+        #endregion
     }
 }

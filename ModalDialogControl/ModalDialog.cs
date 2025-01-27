5c71390d62e99c1a13ce91c159077dd54d979a28
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-01-27 08:53:04 +0000

    Modal Dialog Project for the modal dialog control

---
+++ ModalDialogControl/ModalDialog.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ModalDialogControl
+{
+    /// <summary>
+    ///     Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///     Step 1a) Using this custom control in a XAML file that exists in the current project.
+    ///     Add this XmlNamespace attribute to the root element of the markup file where it is
+    ///     to be used:
+    ///     xmlns:MyNamespace="clr-namespace:ModalDialogControl"
+    ///     Step 1b) Using this custom control in a XAML file that exists in a different project.
+    ///     Add this XmlNamespace attribute to the root element of the markup file where it is
+    ///     to be used:
+    ///     xmlns:MyNamespace="clr-namespace:ModalDialogControl;assembly=ModalDialogControl"
+    ///     You will also need to add a project reference from the project where the XAML file lives
+    ///     to this project and Rebuild to avoid compilation errors:
+    ///     Right-click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Select this project]
+    ///     Step 2)
+    ///     Go ahead and use your control in the XAML file.
+    ///     <MyNamespace:ModalDialog />
+    /// </summary>
+    public class ModalDialog:ContentControl
+    {
+        public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register(
+            nameof(IsOpen),
+            typeof(bool),
+            typeof(ModalDialog),
+            new PropertyMetadata(default(bool)));
+
+        static ModalDialog()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ModalDialog), new FrameworkPropertyMetadata(typeof(ModalDialog)));
+            // BackgroundProperty.OverrideMetadata(typeof(ModalDialog), new FrameworkPropertyMetadata(CreateBackground()));
+
+        }
+
+        public bool IsOpen
+        {
+            get => (bool)GetValue(IsOpenProperty);
+            set => SetValue(IsOpenProperty, value);
+        }
+
+        private static SolidColorBrush CreateBackground()
+        {
+            return new SolidColorBrush(Colors.Black)
+            {
+                Opacity = 0.3
+            };
+        }
+    }
+}

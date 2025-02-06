703a7fd0b39f8fc7d8e3b2ee2e26931c9974a254
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-02-06 00:43:19 +0000

    Removed unused method from the class

--- PiggsCare.Core/Control/ModalNavigationControl.cs
+++ PiggsCare.Core/Control/ModalNavigationControl.cs
@@ -1,7 +1,5 @@
-using MvvmCross;
 using MvvmCross.ViewModels;
 using PiggsCare.Core.Stores;
-using PiggsCare.Core.ViewModels;
 
 namespace PiggsCare.Core.Control
 {
@@ -10,16 +8,6 @@ namespace PiggsCare.Core.Control
         public void PopUp<TViewModel>( int? parameter = null ) where TViewModel : IMvxViewModel
         {
             modalNavigationStore.CurrentModalViewModel = viewModelFactory(typeof(TViewModel), parameter!);
-        }
-
-        public void Open<TViewModel>( int? parameter = null ) where TViewModel : IMvxViewModel
-        {
-            modalNavigationStore.CurrentModalViewModel = ReturnTestViewModel();
-        }
-
-        private TestViewModel? ReturnTestViewModel()
-        {
-            return Mvx.IoCProvider?.Resolve<TestViewModel>();
         }
     }
 }

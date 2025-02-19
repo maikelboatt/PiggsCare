9faa2be010dada63ddc25b9c50119f10f981e183
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-02-19 15:49:46 +0000

    Added a power-off button to the animal details view

--- PiggsCare.Core/ViewModels/SelectedAnimalDetailsViewModel.cs
+++ PiggsCare.Core/ViewModels/SelectedAnimalDetailsViewModel.cs
@@ -1,33 +1,15 @@
+using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using PiggsCare.Core.Factory;
+using PiggsCare.Core.Stores;
+using PiggsCare.Core.ViewModels.HealthRecords;
 
 namespace PiggsCare.Core.ViewModels
 {
-    public class SelectedAnimalDetailsViewModel( IViewModelFactory viewModelFactory ):MvxViewModel<int>, ISelectedAnimalsDetailsViewModel
+    public class SelectedAnimalDetailsViewModel( IViewModelFactory viewModelFactory, ModalNavigationStore modalNavigationStore ):MvxViewModel<int>, ISelectedAnimalsDetailsViewModel
     {
-        public override void Prepare( int parameter )
-        {
-            Console.WriteLine(parameter);
-            SetupViewModel();
-        }
-
-
-        private void SetupViewModel()
-        {
-            HealthListingViewModel? viewmodel = viewModelFactory.CreateViewModel<HealthListingViewModel>();
-            CurrentViewModel = viewmodel;
-            viewmodel?.Initialize();
-        }
-
         #region Fields
 
-        private int _animalId;
-        private int _name;
-        private string _breed;
-        private DateTime _birthDate;
-        private int _certificateNumber;
-        private string _gender;
-        private float _backFatIndex;
         private MvxViewModel? _currentViewModel;
 
         #endregion
@@ -40,37 +22,26 @@ namespace PiggsCare.Core.ViewModels
             set => SetProperty(ref _currentViewModel, value);
         }
 
-        public int Name
+        #endregion
+
+        public MvxCommand PowerOffCommand => new(ExecutePowerOff);
+
+        private void ExecutePowerOff()
         {
-            get => _name;
-            set => SetProperty(ref _name, value);
-        }
-        public string Breed
-        {
-            get => _breed;
-            set => SetProperty(ref _breed, value);
-        }
-        public DateTime BirthDate
-        {
-            get => _birthDate;
-            set => SetProperty(ref _birthDate, value);
-        }
-        public int CertificateNumber
-        {
-            get => _certificateNumber;
-            set => SetProperty(ref _certificateNumber, value);
-        }
-        public string Gender
-        {
-            get => _gender;
-            set => SetProperty(ref _gender, value);
-        }
-        public float BackFatIndex
-        {
-            get => _backFatIndex;
-            set => SetProperty(ref _backFatIndex, value);
+            modalNavigationStore.Close();
         }
 
-        #endregion
+        public override void Prepare( int parameter )
+        {
+            SetupViewModel(parameter);
+        }
+
+
+        private void SetupViewModel( int parameter )
+        {
+            HealthListingViewModel? viewmodel = viewModelFactory.CreateViewModel<HealthListingViewModel, int>(parameter);
+            CurrentViewModel = viewmodel;
+            viewmodel?.Initialize();
+        }
     }
 }

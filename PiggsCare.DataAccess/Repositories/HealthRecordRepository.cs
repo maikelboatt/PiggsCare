8b0bbe12411a0988961ba872d2c269b7f546ec3e
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-02-19 15:47:01 +0000

    Completed all CRUD methods

--- PiggsCare.DataAccess/Repositories/HealthRecordRepository.cs
+++ PiggsCare.DataAccess/Repositories/HealthRecordRepository.cs
@@ -15,9 +15,21 @@ namespace PiggsCare.DataAccess.Repositories
             return result.Select(x => new HealthRecord(x.HealthRecordId, x.AnimalId, x.RecordDate, x.Diagnosis, x.Treatment, x.Outcome));
         }
 
-        public Task<HealthRecord?> GetHealthRecordByIdAsync( int id )
+        public async Task<HealthRecord?> GetHealthRecordByIdAsync( int id )
         {
-            throw new NotImplementedException();
+            // Query the database for any record with matching id
+            IEnumerable<HealthRecordDto> result = await sqlDataAccess.QueryAsync<HealthRecordDto, dynamic>("dbo.GetHealthRecordById", new { HealthRecordId = id }, Connectionstring);
+            HealthRecordDto? healthRecordDto = result.FirstOrDefault();
+
+            // Convert HealthRecordDto to HealthRecord Object
+            return healthRecordDto is not null
+                ? new HealthRecord(healthRecordDto.HealthRecordId,
+                                   healthRecordDto.AnimalId,
+                                   healthRecordDto.RecordDate,
+                                   healthRecordDto.Diagnosis,
+                                   healthRecordDto.Treatment,
+                                   healthRecordDto.Outcome)
+                : null;
         }
 
         public async Task CreateHealthRecordAsync( HealthRecord health )
@@ -42,24 +54,26 @@ namespace PiggsCare.DataAccess.Repositories
                 );
         }
 
-        public Task UpdateHealthRecordAsync( HealthRecord health )
+        public async Task UpdateHealthRecordAsync( HealthRecord health )
         {
-            throw new NotImplementedException();
+            // Convert HealthRecord Object to HealthRecordDto Object
+            HealthRecordDto recordDto = new()
+            {
+                HealthRecordId = health.HealthRecordId,
+                AnimalId = health.AnimalId,
+                RecordDate = health.RecordDate,
+                Diagnosis = health.Diagnosis,
+                Treatment = health.Treatment,
+                Outcome = health.Outcome
+            };
+
+            // Update existing record in the database
+            await sqlDataAccess.CommandAsync("dbo.UpdateHealthRecord", recordDto, Connectionstring);
         }
 
-        public Task DeleteHealthRecordAsync( int id )
+        public async Task DeleteHealthRecordAsync( int id )
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<HealthRecord?> GetHealthRecordByNameAsync( int name )
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<HealthRecord>> GetHealthRecordByBreedAsync( string breed )
-        {
-            throw new NotImplementedException();
+            await sqlDataAccess.CommandAsync("dbo.DeleteHealthRecord", new { HealthRecordId = id }, Connectionstring);
         }
     }
 }

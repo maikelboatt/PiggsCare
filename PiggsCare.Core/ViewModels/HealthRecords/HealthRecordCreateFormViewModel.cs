452d4de8aefa24251e57ec86977465c5ceefe352
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-01 01:20:42 +0000

    Added validation functionality to the viewmodels

--- PiggsCare.Core/ViewModels/HealthRecords/HealthRecordCreateFormViewModel.cs
+++ PiggsCare.Core/ViewModels/HealthRecords/HealthRecordCreateFormViewModel.cs
@@ -1,12 +1,49 @@
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using PiggsCare.Core.Stores;
+using PiggsCare.Core.Validation;
 using PiggsCare.Domain.Models;
+using System.Collections;
+using System.ComponentModel;
 
 namespace PiggsCare.Core.ViewModels.HealthRecords
 {
-    public class HealthRecordCreateFormViewModel( IHealthRecordStore healthRecordStore, ModalNavigationStore modalNavigationStore ):MvxViewModel<int>, IHealthRecordCreateFormViewModel
+    public class HealthRecordCreateFormViewModel:MvxViewModel<int>, IHealthRecordCreateFormViewModel, INotifyDataErrorInfo
     {
+        #region Constructor
+
+        public HealthRecordCreateFormViewModel( IHealthRecordStore healthRecordStore, ModalNavigationStore modalNavigationStore, IHealthRecordValidation recordValidation )
+        {
+            _healthRecordStore = healthRecordStore;
+            _modalNavigationStore = modalNavigationStore;
+            _recordValidation = recordValidation;
+            _recordValidation.Errors.Clear();
+
+            recordValidation.ErrorsChanged += RecordValidationOnErrorsChanged;
+        }
+
+        #endregion
+
+        #region INotifyDataErrorInfo Implementation
+
+        public IEnumerable GetErrors( string? propertyName )
+        {
+            return _recordValidation.GetErrors(propertyName);
+        }
+
+        public bool HasErrors => _recordValidation.HasErrors;
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        private void RecordValidationOnErrorsChanged( object? sender, DataErrorsChangedEventArgs e )
+        {
+            ErrorsChanged?.Invoke(this, e);
+            RaisePropertyChanged(nameof(HasErrors));
+            RaisePropertyChanged(nameof(CanSubmitRecord));
+        }
+
+        #endregion
+
+
         #region ViewModel Life-Cycle
 
         public override void Prepare( int parameter )
@@ -14,47 +51,85 @@ namespace PiggsCare.Core.ViewModels.HealthRecords
             _animalId = parameter;
         }
 
-        #endregion
+        public override void ViewDestroy( bool viewFinishing = true )
+        {
+            base.ViewDestroy(viewFinishing);
+            _recordValidation.ErrorsChanged -= RecordValidationOnErrorsChanged;
+        }
 
+        #endregion
 
         #region Fields
 
-        private DateTime _recordDate;
-        private string _diagnosis;
-        private string _treatment;
-        private string _outcome;
+        private DateOnly _recordDate;
+        private string _diagnosis = string.Empty;
+        private string _treatment = string.Empty;
+        private string _outcome = string.Empty;
         private int _animalId;
+        private readonly IHealthRecordStore _healthRecordStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+        private readonly IHealthRecordValidation _recordValidation;
 
         #endregion
 
         #region Properties
 
-        public DateTime RecordDate
+        public DateOnly RecordDate
         {
             get => _recordDate;
-            set => SetProperty(ref _recordDate, value);
+            set
+            {
+                if (value.Equals(_recordDate)) return;
+                _recordDate = value;
+                _recordValidation.ValidateProp(value);
+                RaisePropertyChanged();
+            }
         }
+
         public string Diagnosis
         {
             get => _diagnosis;
-            set => SetProperty(ref _diagnosis, value);
+            set
+            {
+                if (value.Equals(_diagnosis)) return;
+                _diagnosis = value;
+                _recordValidation.ValidateProp(value);
+                RaisePropertyChanged();
+            }
         }
         public string Treatment
         {
             get => _treatment;
-            set => SetProperty(ref _treatment, value);
+            set
+            {
+                if (value.Equals(_treatment)) return;
+                _treatment = value;
+                _recordValidation.ValidateProp(_treatment);
+                RaisePropertyChanged();
+            }
         }
         public string Outcome
         {
             get => _outcome;
-            set => SetProperty(ref _outcome, value);
+            set
+            {
+                if (value.Equals(_outcome)) return;
+                _outcome = value;
+                _recordValidation.ValidateProp(_outcome);
+                RaisePropertyChanged();
+            }
         }
 
         #endregion
 
         #region Commands
 
-        public IMvxAsyncCommand SubmitRecordCommand => new MvxAsyncCommand(ExecuteSubmitRecord);
+        public IMvxAsyncCommand SubmitRecordCommand => new MvxAsyncCommand(ExecuteSubmitRecord, CanSubmitRecord);
+
+        private bool CanSubmitRecord()
+        {
+            return !HasErrors;
+        }
 
         public IMvxCommand CancelRecordCommand => new MvxCommand(ExecuteCancelCommand);
 
@@ -65,20 +140,19 @@ namespace PiggsCare.Core.ViewModels.HealthRecords
         private async Task ExecuteSubmitRecord()
         {
             HealthRecord record = GetHealthRecordFromFields();
-            await healthRecordStore.Create(record);
-            modalNavigationStore.Close();
+            await _healthRecordStore.Create(record);
+            _modalNavigationStore.Close();
         }
 
 
         private void ExecuteCancelCommand()
         {
-            modalNavigationStore.Close();
+            _modalNavigationStore.Close();
         }
 
         private HealthRecord GetHealthRecordFromFields()
         {
-            return new HealthRecord(1, _animalId, DateTime.Today, _diagnosis, _treatment, _outcome);
-            // Todo: use inserted date in both create instead of the DateTime object
+            return new HealthRecord(1, _animalId, RecordDate, _diagnosis, _treatment, _outcome);
         }
 
         #endregion
--- PiggsCare.Core/ViewModels/HealthRecords/HealthRecordModifyFormViewModel.cs
+++ PiggsCare.Core/ViewModels/HealthRecords/HealthRecordModifyFormViewModel.cs
@@ -1,12 +1,46 @@
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using PiggsCare.Core.Stores;
+using PiggsCare.Core.Validation;
 using PiggsCare.Domain.Models;
+using System.Collections;
+using System.ComponentModel;
 
 namespace PiggsCare.Core.ViewModels.HealthRecords
 {
-    public class HealthRecordModifyFormViewModel( IHealthRecordStore healthRecordStore, ModalNavigationStore modalNavigationStore ):MvxViewModel<int>, IHealthRecordModifyFormViewModel
+    public class HealthRecordModifyFormViewModel:MvxViewModel<int>, IHealthRecordModifyFormViewModel, INotifyDataErrorInfo
     {
+        public IEnumerable GetErrors( string? propertyName )
+        {
+            return _recordValidation.GetErrors(propertyName);
+        }
+
+        public bool HasErrors => _recordValidation.HasErrors;
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        #region Constructor
+
+        public HealthRecordModifyFormViewModel( IHealthRecordStore healthRecordStore, ModalNavigationStore modalNavigationStore, IHealthRecordValidation recordValidation )
+        {
+            _healthRecordStore = healthRecordStore;
+            _modalNavigationStore = modalNavigationStore;
+            _recordValidation = recordValidation;
+            _recordValidation.Errors.Clear();
+
+            recordValidation.ErrorsChanged += RecordValidationOnErrorsChanged;
+        }
+
+        private void RecordValidationOnErrorsChanged( object? sender, DataErrorsChangedEventArgs e )
+        {
+            ErrorsChanged?.Invoke(this, e);
+            RaisePropertyChanged(nameof(HasErrors));
+            RaisePropertyChanged(nameof(CanSubmitRecord));
+        }
+
+        #endregion
+
+        #region ViewModel Life-Cycle
+
         public override void Prepare( int parameter )
         {
             _healthRecordId = parameter;
@@ -14,11 +48,92 @@ namespace PiggsCare.Core.ViewModels.HealthRecords
 
         public override Task Initialize()
         {
-            HealthRecord? record = healthRecordStore?.HealthRecords.FirstOrDefault(x => x.HealthRecordId == _healthRecordId);
+            HealthRecord? record = _healthRecordStore?.HealthRecords.FirstOrDefault(x => x.HealthRecordId == _healthRecordId);
             if (record == null) return base.Initialize();
             PopulateEditForm(record);
             _animalId = record.AnimalId;
             return base.Initialize();
+        }
+
+        #endregion
+
+        #region Fields
+
+        private DateOnly _recordDate;
+        private string _diagnosis = string.Empty;
+        private string _treatment = string.Empty;
+        private string _outcome = string.Empty;
+        private int _healthRecordId;
+        private int _animalId;
+        private readonly IHealthRecordStore _healthRecordStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+        private readonly IHealthRecordValidation _recordValidation;
+
+        #endregion
+
+        #region Properties
+
+        public DateOnly RecordDate
+        {
+            get => _recordDate;
+            set
+            {
+                if (value.Equals(_recordDate)) return;
+                _recordDate = value;
+                _recordValidation.ValidateProp(value);
+                RaisePropertyChanged();
+            }
+        }
+        public string Diagnosis
+        {
+            get => _diagnosis;
+            set
+            {
+                if (value.Equals(_diagnosis)) return;
+                _diagnosis = value;
+                _recordValidation.ValidateProp(value);
+                RaisePropertyChanged();
+            }
+        }
+        public string Treatment
+        {
+            get => _treatment;
+            set
+            {
+                if (value.Equals(_treatment)) return;
+                _treatment = value;
+                _recordValidation.ValidateProp(_treatment);
+                RaisePropertyChanged();
+            }
+        }
+        public string Outcome
+        {
+            get => _outcome;
+            set
+            {
+                if (value.Equals(_outcome)) return;
+                _outcome = value;
+                _recordValidation.ValidateProp(_outcome);
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public IMvxAsyncCommand SubmitRecordCommand => new MvxAsyncCommand(ExecuteSubmitRecord, CanSubmitRecord);
+
+
+        public IMvxCommand CancelRecordCommand => new MvxCommand(ExecuteCancelCommand);
+
+        #endregion
+
+        #region Methods
+
+        private bool CanSubmitRecord()
+        {
+            return !HasErrors;
         }
 
         private void PopulateEditForm( HealthRecord record )
@@ -29,68 +144,22 @@ namespace PiggsCare.Core.ViewModels.HealthRecords
             _outcome = record.Outcome;
         }
 
-        #region Fields
-
-        private DateTime _recordDate;
-        private string _diagnosis;
-        private string _treatment;
-        private string _outcome;
-        private int _healthRecordId;
-        private int _animalId;
-
-        #endregion
-
-        #region Properties
-
-        public DateTime RecordDate
-        {
-            get => _recordDate;
-            set => SetProperty(ref _recordDate, value);
-        }
-        public string Diagnosis
-        {
-            get => _diagnosis;
-            set => SetProperty(ref _diagnosis, value);
-        }
-        public string Treatment
-        {
-            get => _treatment;
-            set => SetProperty(ref _treatment, value);
-        }
-        public string Outcome
-        {
-            get => _outcome;
-            set => SetProperty(ref _outcome, value);
-        }
-
-        #endregion
-
-        #region Commands
-
-        public IMvxAsyncCommand SubmitRecordCommand => new MvxAsyncCommand(ExecuteSubmitRecord);
-
-        public IMvxCommand CancelRecordCommand => new MvxCommand(ExecuteCancelCommand);
-
-        #endregion
-
-        #region Methods
-
         private async Task ExecuteSubmitRecord()
         {
             HealthRecord record = GetHealthRecordFromFields();
-            await healthRecordStore.Modify(record);
-            modalNavigationStore.Close();
+            await _healthRecordStore.Modify(record);
+            _modalNavigationStore.Close();
         }
 
         private HealthRecord GetHealthRecordFromFields()
         {
-            return new HealthRecord(_healthRecordId, _animalId, DateTime.Today, _diagnosis, _treatment, _outcome);
+            return new HealthRecord(_healthRecordId, _animalId, RecordDate, _diagnosis, _treatment, _outcome);
             // Todo: use inserted date in both create instead of the DateTime object
         }
 
         private void ExecuteCancelCommand()
         {
-            modalNavigationStore.Close();
+            _modalNavigationStore.Close();
         }
 
         #endregion

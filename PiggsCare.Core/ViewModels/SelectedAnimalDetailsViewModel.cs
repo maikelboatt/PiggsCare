cf048f96ba0dd1c6bba08de1c5ab7e06bb14c18f
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-01 01:23:19 +0000

    Added navigation functionality to the SelectedAnimalDetails

--- PiggsCare.Core/ViewModels/SelectedAnimalDetailsViewModel.cs
+++ PiggsCare.Core/ViewModels/SelectedAnimalDetailsViewModel.cs
@@ -2,18 +2,13 @@ using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using PiggsCare.Core.Factory;
 using PiggsCare.Core.Stores;
+using PiggsCare.Core.ViewModels.Breeding;
 using PiggsCare.Core.ViewModels.HealthRecords;
 
 namespace PiggsCare.Core.ViewModels
 {
     public class SelectedAnimalDetailsViewModel( IViewModelFactory viewModelFactory, ModalNavigationStore modalNavigationStore ):MvxViewModel<int>, ISelectedAnimalsDetailsViewModel
     {
-        #region Fields
-
-        private MvxViewModel? _currentViewModel;
-
-        #endregion
-
         #region Properties
 
         public MvxViewModel? CurrentViewModel
@@ -33,15 +28,41 @@ namespace PiggsCare.Core.ViewModels
 
         public override void Prepare( int parameter )
         {
-            SetupViewModel(parameter);
+            _animalId = parameter;
+            ExecuteNavigateToHealth();
         }
 
+        #region Fields
 
-        private void SetupViewModel( int parameter )
+        private MvxViewModel? _currentViewModel;
+        private int _animalId;
+
+        #endregion
+
+
+        #region Commands
+
+        public IMvxCommand NavigateToHealth => new MvxCommand(ExecuteNavigateToHealth);
+        public IMvxCommand NavigateToBreeding => new MvxCommand(ExecuteNavigateToBreeding);
+
+        #endregion
+
+        #region Methods
+
+        private void ExecuteNavigateToHealth()
         {
-            HealthListingViewModel? viewmodel = viewModelFactory.CreateViewModel<HealthListingViewModel, int>(parameter);
+            HealthListingViewModel? viewmodel = viewModelFactory.CreateViewModel<HealthListingViewModel, int>(_animalId);
             CurrentViewModel = viewmodel;
             viewmodel?.Initialize();
         }
+
+        private void ExecuteNavigateToBreeding()
+        {
+            BreedingEventListingViewModel? viewmodel = viewModelFactory.CreateViewModel<BreedingEventListingViewModel, int>(_animalId);
+            CurrentViewModel = viewmodel;
+            viewmodel?.Initialize();
+        }
+
+        #endregion
     }
 }

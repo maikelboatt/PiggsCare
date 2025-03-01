9f2f6df998712f5adcd0e019fc400972ab19fbed
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-01 01:10:28 +0000

    Overriding the default ToString method

--- PiggsCare.Domain/Models/BreedingEvent.cs
+++ PiggsCare.Domain/Models/BreedingEvent.cs
@@ -1,10 +1,15 @@
 namespace PiggsCare.Domain.Models
 {
-    public class BreedingEvent( int breedingEventId, int animalId, DateTime aiDate, DateTime? expectedFarrowDate )
+    public class BreedingEvent( int breedingEventId, int animalId, DateOnly aiDate, DateOnly expectedFarrowDate )
     {
         public int BreedingEventId { get; init; } = breedingEventId;
         public int AnimalId { get; init; } = animalId;
-        public DateTime AiDate { get; init; } = aiDate;
-        public DateTime? ExpectedFarrowDate { get; init; } = expectedFarrowDate; // Nullable DateTime
+        public DateOnly AiDate { get; init; } = aiDate;
+        public DateOnly ExpectedFarrowDate { get; init; } = expectedFarrowDate; // Nullable DateTime
+
+        public override string ToString()
+        {
+            return $"Breeding Event Id: {BreedingEventId}, Animal Id: {AnimalId}, AiDate: {AiDate}, Expected FarrowDate: {ExpectedFarrowDate}";
+        }
     }
 }

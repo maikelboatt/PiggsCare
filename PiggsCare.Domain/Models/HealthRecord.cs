7b988d72627f397b496d0612a587d4fad9d40465
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-01 01:19:30 +0000

    Changed RecordDate prop from DateTime to DateOnly

--- PiggsCare.Domain/Models/HealthRecord.cs
+++ PiggsCare.Domain/Models/HealthRecord.cs
@@ -3,14 +3,14 @@ namespace PiggsCare.Domain.Models
     public class HealthRecord(
         int healthRecordId,
         int animalId,
-        DateTime recordDate,
+        DateOnly recordDate,
         string diagnosis,
         string treatment,
         string outcome )
     {
         public int HealthRecordId { get; init; } = healthRecordId;
         public int AnimalId { get; init; } = animalId;
-        public DateTime RecordDate { get; init; } = recordDate;
+        public DateOnly RecordDate { get; init; } = recordDate;
         public string Diagnosis { get; init; } = diagnosis;
         public string Treatment { get; init; } = treatment;
         public string Outcome { get; init; } = outcome;

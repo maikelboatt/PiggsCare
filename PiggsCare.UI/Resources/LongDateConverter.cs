f99cd8f9518981614ac4945b095ff9d6b543c4e3
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-01 01:08:15 +0000

    Date Converters and Null to boolean Converters

---
+++ PiggsCare.UI/Resources/LongDateConverter.cs
@@ -0,0 +1,24 @@
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PiggsCare.UI.Resources
+{
+    public class LongDateConverter:IValueConverter
+    {
+        public object? Convert( object? value, Type targetType, object? parameter, CultureInfo culture )
+        {
+            if (value is DateOnly dateOnly)
+            {
+                // Format the date as a long date string
+                return dateOnly.ToString("D", culture);
+            }
+
+            return value;
+        }
+
+        public object? ConvertBack( object? value, Type targetType, object? parameter, CultureInfo culture )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

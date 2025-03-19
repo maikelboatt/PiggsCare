9e6462f07ad7ff0d0b226b0fb3ba263943d43060
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-19 20:30:35 +0000

    A DTO for synchronization events

--- PiggsCare.DataAccess/DTO/EstrusSynchronizationEventDto.cs
+++ PiggsCare.DataAccess/DTO/EstrusSynchronizationEventDto.cs
@@ -2,11 +2,16 @@ namespace PiggsCare.DataAccess.DTO
 {
     public class EstrusSynchronizationEventDto
     {
-        public int EstrusSynchronizationEventId { get; init; }
+        public int SynchronizationEventId { get; init; }
         public required DateTime StartDate { get; init; }
         public required DateTime EndDate { get; set; }
         public required string BatchNumber { get; set; }
+        public required string SynchronizationProtocol { get; set; }
+        public required string Comments { get; set; }
 
-        // public List<int> AnimalIds { get; init; } = [];
+
+        // Hybrid approach: link to associated breeding events
+        // public List<BreedingEventDto>? BreedingEvents { get; init; }
     }
+    // public List<int> AnimalIds { get; init; } = [];
 }

375f5a9241e909553994248bb04a179d817ccf82
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-19 20:34:15 +0000

    UI Forms and components of the farrow event for listing, creating, modifying and deleting

---
+++ PiggsCare.UI/Forms/Farrowing/FarrowingCreateForm.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace PiggsCare.UI.Forms.Farrowing
+{
+    public partial class FarrowingCreateForm:MvxWpfView
+    {
+        public FarrowingCreateForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PiggsCare.UI/Forms/Farrowing/FarrowingModifyForm.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace PiggsCare.UI.Forms.Farrowing
+{
+    public partial class FarrowingModifyForm:MvxWpfView
+    {
+        public FarrowingModifyForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

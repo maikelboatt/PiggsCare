ec762d3f8f8a60ec5382760edbba65eac02096b1
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-19 20:44:53 +0000

    UI Forms and Components of the removal event for listing, creating, modifying and deleting

---
+++ PiggsCare.UI/Components/RemovalListing.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace PiggsCare.UI.Components
+{
+    public partial class RemovalListing:MvxWpfView
+    {
+        public RemovalListing()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PiggsCare.UI/Forms/Removal/RemovalEventDeleteForm.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace PiggsCare.UI.Forms.Removal
+{
+    public partial class RemovalEventDeleteForm:MvxWpfView
+    {
+        public RemovalEventDeleteForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

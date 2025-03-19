c82d9b08b88e220287d00717409efc0cd903ddb7
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-19 21:15:08 +0000

    Updated the viewmodel and view for better UX

--- PiggsCare.Core/ViewModels/SelectedAnimalDetailsViewModel.cs
+++ PiggsCare.Core/ViewModels/SelectedAnimalDetailsViewModel.cs
@@ -4,17 +4,27 @@ using PiggsCare.Core.Factory;
 using PiggsCare.Core.Stores;
 using PiggsCare.Core.ViewModels.Breeding;
 using PiggsCare.Core.ViewModels.HealthRecords;
+using PiggsCare.Core.ViewModels.Removal;
+using PiggsCare.Core.ViewModels.Synchronization;
+using PiggsCare.Domain.Services;
+using System.Windows;
 
 namespace PiggsCare.Core.ViewModels
 {
-    public class SelectedAnimalDetailsViewModel( IViewModelFactory viewModelFactory, ModalNavigationStore modalNavigationStore ):MvxViewModel<int>, ISelectedAnimalsDetailsViewModel
+    public class SelectedAnimalDetailsViewModel:MvxViewModel<int>, ISelectedAnimalsDetailsViewModel
     {
-        #region Properties
+        #region Constructor
 
-        public MvxViewModel? CurrentViewModel
+        public SelectedAnimalDetailsViewModel( IViewModelFactory viewModelFactory, ModalNavigationStore modalNavigationStore, ICurrentViewModelStore currentViewModelStore,
+            IMessageService messageService )
         {
-            get => _currentViewModel;
-            set => SetProperty(ref _currentViewModel, value);
+            _viewModelFactory = viewModelFactory;
+            _modalNavigationStore = modalNavigationStore;
+            _currentViewModelStore = currentViewModelStore;
+            _messageService = messageService;
+
+            _currentViewModelStore.CurrentViewModelChanged += CurrentViewModelStoreOnCurrentViewModelChanged;
+            _currentViewModelStore.CurrentProcessStageChanged += CurrentViewModelStoreOnCurrentProcessStageChanged;
         }
 
         #endregion
@@ -23,8 +33,48 @@ namespace PiggsCare.Core.ViewModels
 
         private void ExecutePowerOff()
         {
-            modalNavigationStore.Close();
+            _modalNavigationStore.Close();
         }
+
+        #region Event Handler
+
+        private void CurrentViewModelStoreOnCurrentViewModelChanged()
+        {
+            RaisePropertyChanged(nameof(CurrentViewModel));
+        }
+
+        private void CurrentViewModelStoreOnCurrentProcessStageChanged()
+        {
+            RaisePropertyChanged(nameof(CurrentProcessStage));
+        }
+
+        #endregion
+
+        #region Properties
+
+        public MvxViewModel? CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                if (value == _currentViewModel) return;
+                _currentViewModelStore.CurrentViewModel = value;
+            }
+        }
+
+        public ProcessStage CurrentProcessStage
+        {
+            get => _currentStage;
+            set
+            {
+                if (value == _currentStage) return;
+                _currentViewModelStore.CurrentProcessStage = value;
+            }
+        }
+
+        #endregion
+
+        #region ViewModel Life-Cycle
 
         public override void Prepare( int parameter )
         {
@@ -32,18 +82,36 @@ namespace PiggsCare.Core.ViewModels
             ExecuteNavigateToHealth();
         }
 
-        #region Fields
-
-        private MvxViewModel? _currentViewModel;
-        private int _animalId;
+        public override void ViewDestroy( bool viewFinishing = true )
+        {
+            base.ViewDestroy(viewFinishing);
+            _currentViewModelStore.CurrentViewModelChanged -= CurrentViewModelStoreOnCurrentViewModelChanged;
+            _currentViewModelStore.CurrentProcessStageChanged -= CurrentViewModelStoreOnCurrentProcessStageChanged;
+        }
 
         #endregion
 
+        #region Fields
+
+        private MvxViewModel? _currentViewModel => _currentViewModelStore?.CurrentViewModel;
+        private ProcessStage _currentStage => _currentViewModelStore.CurrentProcessStage;
+        private int _animalId;
+        private readonly IViewModelFactory _viewModelFactory;
+        private readonly ModalNavigationStore _modalNavigationStore;
+        private readonly ICurrentViewModelStore _currentViewModelStore;
+        private readonly IMessageService _messageService;
+
+        #endregion
 
         #region Commands
 
         public IMvxCommand NavigateToHealth => new MvxCommand(ExecuteNavigateToHealth);
+        public IMvxCommand NavigateToSynchronization => new MvxCommand(ExecuteNavigateToSynchronization);
         public IMvxCommand NavigateToBreeding => new MvxCommand(ExecuteNavigateToBreeding);
+        public IMvxCommand NavigateToFarrowing => new MvxCommand(ExecuteNavigateToFarrowing);
+        public IMvxCommand NavigateToPregnancy => new MvxCommand(ExecuteNavigateToPregnancy);
+        public IMvxCommand NavigateToWeaning => new MvxCommand(ExecuteNavigateToWeaning);
+        public IMvxCommand NavigateToRemoval => new MvxCommand(ExecuteNavigateToRemoval);
 
         #endregion
 
@@ -51,15 +119,78 @@ namespace PiggsCare.Core.ViewModels
 
         private void ExecuteNavigateToHealth()
         {
-            HealthListingViewModel? viewmodel = viewModelFactory.CreateViewModel<HealthListingViewModel, int>(_animalId);
-            CurrentViewModel = viewmodel;
+            HealthListingViewModel? viewmodel = _viewModelFactory.CreateViewModel<HealthListingViewModel, int>(_animalId);
+            _currentViewModelStore.CurrentViewModel = viewmodel;
+            CurrentProcessStage = ProcessStage.Health;
+            viewmodel?.Initialize();
+        }
+
+        private void ExecuteNavigateToSynchronization()
+        {
+            SynchronizationListingViewModel? viewmodel = _viewModelFactory.CreateViewModel<SynchronizationListingViewModel, int>(_animalId);
+            _currentViewModelStore.CurrentViewModel = viewmodel;
+            CurrentProcessStage = ProcessStage.Synchronization;
             viewmodel?.Initialize();
         }
 
         private void ExecuteNavigateToBreeding()
         {
-            BreedingEventListingViewModel? viewmodel = viewModelFactory.CreateViewModel<BreedingEventListingViewModel, int>(_animalId);
-            CurrentViewModel = viewmodel;
+            BreedingEventListingViewModel? viewmodel = _viewModelFactory.CreateViewModel<BreedingEventListingViewModel, int>(_animalId);
+            _currentViewModelStore.CurrentViewModel = viewmodel;
+            CurrentProcessStage = ProcessStage.Breeding;
+            viewmodel?.Initialize();
+        }
+
+        private void ExecuteNavigateToFarrowing()
+        {
+            if (_messageService.Show(
+                    "Please right-click on a Insemination event and choose Farrowing from the options to successfully view data on farrowing",
+                    "Information",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information) != MessageBoxResult.OK)
+            {
+                return;
+            }
+
+            CurrentProcessStage = ProcessStage.Farrowing;
+            ExecuteNavigateToBreeding();
+        }
+
+        private void ExecuteNavigateToPregnancy()
+        {
+            if (_messageService.Show(
+                    "Please right-click on a Insemination event and choose Pregnancy from the options to successfully view data on pregnancy",
+                    "Information",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information) != MessageBoxResult.OK)
+            {
+                return;
+            }
+
+            CurrentProcessStage = ProcessStage.Pregnancy;
+            ExecuteNavigateToBreeding();
+        }
+
+        private void ExecuteNavigateToWeaning()
+        {
+            if (_messageService.Show(
+                    "Please right-click on a Farrowing event and choose Weaning from the options to successfully view data on weaning",
+                    "Information",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information) != MessageBoxResult.OK)
+            {
+                return;
+            }
+
+            CurrentProcessStage = ProcessStage.Weaning;
+            ExecuteNavigateToFarrowing();
+        }
+
+        private void ExecuteNavigateToRemoval()
+        {
+            RemovalListingViewModel? viewmodel = _viewModelFactory.CreateViewModel<RemovalListingViewModel, int>(_animalId);
+            _currentViewModelStore.CurrentViewModel = viewmodel;
+            CurrentProcessStage = ProcessStage.Removal;
             viewmodel?.Initialize();
         }
 

9d34f404e423a51f48e3afa3e2e3eb9cd087a31d
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-19 21:11:58 +0000

    Added a nullable SynchronizationEventId to the breeding Model

--- PiggsCare.Domain/Models/BreedingEvent.cs
+++ PiggsCare.Domain/Models/BreedingEvent.cs
@@ -1,11 +1,13 @@
 namespace PiggsCare.Domain.Models
 {
-    public class BreedingEvent( int breedingEventId, int animalId, DateOnly aiDate, DateOnly expectedFarrowDate )
+    public class BreedingEvent( int breedingEventId, int animalId, DateOnly aiDate, DateOnly expectedFarrowDate, int? synchronizationEventId )
     {
         public int BreedingEventId { get; init; } = breedingEventId;
         public int AnimalId { get; init; } = animalId;
         public DateOnly AiDate { get; init; } = aiDate;
-        public DateOnly ExpectedFarrowDate { get; init; } = expectedFarrowDate; // Nullable DateTime
+        public DateOnly ExpectedFarrowDate { get; init; } = expectedFarrowDate;
+
+        public int? SynchronizationEventId { get; set; } = synchronizationEventId;
 
         public override string ToString()
         {

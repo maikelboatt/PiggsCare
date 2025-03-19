fb1296d00cb4881cf6636ac78315c393a5454656
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-19 20:52:47 +0000

    Enum for the various event a pig can partake in

---
+++ PiggsCare.Core/ViewModels/ProcessStage.cs
@@ -0,0 +1,13 @@
+namespace PiggsCare.Core.ViewModels
+{
+    public enum ProcessStage
+    {
+        Health,
+        Breeding,
+        Farrowing,
+        Pregnancy,
+        Weaning,
+        Synchronization,
+        Removal
+    }
+}

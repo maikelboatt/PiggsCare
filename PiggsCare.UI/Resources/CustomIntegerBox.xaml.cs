80332810e6cd73ac90769451cdf3a56d29e95319
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-19 20:29:07 +0000

    A custom control for integer box for integers used through-out the application

---
+++ PiggsCare.UI/Resources/CustomIntegerBox.xaml.cs
@@ -0,0 +1,61 @@
+using MvvmCross.Platforms.Wpf.Views;
+using System.Windows;
+
+namespace PiggsCare.UI.Resources
+{
+    public partial class CustomIntegerBox:MvxWpfView
+    {
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register(
+            nameof(Data),
+            typeof(string),
+            typeof(CustomIntegerBox),
+            new PropertyMetadata(default(string)));
+
+        public static readonly DependencyProperty BiggestProperty = DependencyProperty.Register(
+            nameof(Biggest),
+            typeof(int),
+            typeof(CustomIntegerBox),
+            new PropertyMetadata(default(int)));
+
+        public static readonly DependencyProperty SmallestProperty = DependencyProperty.Register(
+            nameof(Smallest),
+            typeof(int),
+            typeof(CustomIntegerBox),
+            new PropertyMetadata(default(int)));
+
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+            nameof(Description),
+            typeof(string),
+            typeof(CustomIntegerBox),
+            new PropertyMetadata(default(string)));
+
+        public CustomIntegerBox()
+        {
+            InitializeComponent();
+        }
+
+        public int Biggest
+        {
+            get => (int)GetValue(BiggestProperty);
+            set => SetValue(BiggestProperty, value);
+        }
+
+        public int Smallest
+        {
+            get => (int)GetValue(SmallestProperty);
+            set => SetValue(SmallestProperty, value);
+        }
+
+        public string Data
+        {
+            get => (string)GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+    }
+}

8ed858b192a0e322d8b1e155618389287b930c9f
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-03-28 09:29:50 +0000

    Views to display synchronizations in different sizes

---
+++ PiggsCare.UI/Views/Synchronization.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace PiggsCare.UI.Views
+{
+    public partial class Synchronization:MvxWpfView
+    {
+        public Synchronization()
+        {
+            InitializeComponent();
+        }
+    }
+}

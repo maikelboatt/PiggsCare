71220c3e8d87671137ef3a04f4634c8179f45323
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-08-19 15:58:20 +0000

    Code-behind for the notification reminder views

---
+++ PiggsCare.UI/Components/ScheduleNotificationListing.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace PiggsCare.UI.Components
+{
+    public partial class ScheduleNotificationListing:MvxWpfView
+    {
+        public ScheduleNotificationListing()
+        {
+            InitializeComponent();
+        }
+    }
+}

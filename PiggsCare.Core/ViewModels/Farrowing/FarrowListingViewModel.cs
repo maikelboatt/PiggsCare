96bbb40ddc0ad3094e39b8836f733182bdaf2ac0
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-08-19 15:36:09 +0000

    Refactor FarrowListingViewModel to improve event handling and logging

--- PiggsCare.Core/ViewModels/Farrowing/FarrowListingViewModel.cs
+++ PiggsCare.Core/ViewModels/Farrowing/FarrowListingViewModel.cs
@@ -1,37 +1,52 @@
+using Microsoft.Extensions.Logging;
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
+using PiggsCare.ApplicationState.Stores;
+using PiggsCare.ApplicationState.Stores.Farrowing;
+using PiggsCare.Business.Services.Farrowing;
 using PiggsCare.Core.Control;
 using PiggsCare.Core.Factory;
-using PiggsCare.Core.Stores;
 using PiggsCare.Core.ViewModels.Weaning;
 using PiggsCare.Domain.Models;
+using PiggsCare.Infrastructure.Enums;
 using System.Collections.Specialized;
 
 namespace PiggsCare.Core.ViewModels.Farrowing
 {
     public class FarrowListingViewModel:MvxViewModel<int>, IFarrowListingViewModel
     {
+        private readonly ICurrentViewModelStore _currentViewModelStore;
+
+
+        private readonly MvxObservableCollection<FarrowEvent> _farrowEvents;
+        private readonly IFarrowingService _farrowingService;
+        private readonly IFarrowingStore _farrowingStore;
+        private readonly ILogger<FarrowListingViewModel> _logger;
+        private readonly IModalNavigationControl _modalNavigationControl;
+        private readonly IViewModelFactory _viewModelFactory;
+
+        private bool _isLoading;
+
+
         #region Constructor
 
-        public FarrowListingViewModel( IFarrowingStore farrowingStore, IModalNavigationControl modalNavigationControl, IViewModelFactory viewModelFactory,
-            ICurrentViewModelStore currentViewModelStore )
+        public FarrowListingViewModel( IFarrowingService farrowingService, IFarrowingStore farrowingStore, IModalNavigationControl modalNavigationControl,
+            IViewModelFactory viewModelFactory,
+            ICurrentViewModelStore currentViewModelStore, ILogger<FarrowListingViewModel> logger )
         {
+            _farrowingService = farrowingService;
             _farrowingStore = farrowingStore;
             _modalNavigationControl = modalNavigationControl;
             _viewModelFactory = viewModelFactory;
             _currentViewModelStore = currentViewModelStore;
-            _farrowEvents = new MvxObservableCollection<FarrowEvent>(_farrowingStore.FarrowEvents);
+            _logger = logger;
+            _farrowEvents = new MvxObservableCollection<FarrowEvent>(_farrowingStore.Farrowings);
 
             _farrowEvents.CollectionChanged += FarrowEventsOnCollectionChanged;
-        }
 
-        #endregion
-
-        #region Event Handler
-
-        private void FarrowEventsOnCollectionChanged( object? sender, NotifyCollectionChangedEventArgs e )
-        {
-            RaisePropertyChanged(nameof(FarrowEvents));
+            _farrowingStore.OnFarrowingAdded += FarrowingStoreOnFarrowingAdded;
+            _farrowingStore.OnFarrowingUpdated += FarrowingStoreOnOnFarrowingUpdated;
+            _farrowingStore.OnFarrowingDeleted += FarrowingStoreOnFarrowingDeleted;
         }
 
         #endregion
@@ -49,18 +64,47 @@ namespace PiggsCare.Core.ViewModels.Farrowing
             await base.Initialize();
         }
 
+        public override void ViewDestroy( bool viewFinishing = true )
+        {
+            _farrowingStore.OnFarrowingAdded -= FarrowingStoreOnFarrowingAdded;
+            _farrowingStore.OnFarrowingUpdated -= FarrowingStoreOnOnFarrowingUpdated;
+            _farrowingStore.OnFarrowingDeleted -= FarrowingStoreOnFarrowingDeleted;
+            base.ViewDestroy(viewFinishing);
+        }
+
         #endregion
 
-        #region Fields
+        #region Event Handler
 
-        private bool _isLoading;
-        private readonly IFarrowingStore _farrowingStore;
-        private readonly IModalNavigationControl _modalNavigationControl;
-        private readonly IViewModelFactory _viewModelFactory;
-        private readonly ICurrentViewModelStore _currentViewModelStore;
+        private void FarrowEventsOnCollectionChanged( object? sender, NotifyCollectionChangedEventArgs e )
+        {
+            RaisePropertyChanged(nameof(FarrowEvents));
+        }
 
+        private void FarrowingStoreOnFarrowingDeleted( FarrowEvent farrow )
+        {
+            _farrowEvents.Remove(farrow);
+        }
 
-        private readonly MvxObservableCollection<FarrowEvent> _farrowEvents;
+        private void FarrowingStoreOnOnFarrowingUpdated( FarrowEvent farrow )
+        {
+            // Update the animal in the local collection if it exists
+            int index = _farrowEvents.IndexOf(farrow);
+            if (index >= 0)
+            {
+                _farrowEvents.RemoveAt(index);
+                _farrowEvents.Insert(index, farrow);
+            }
+            else
+            {
+                _logger.LogWarning("Animal with ID {FarrowId} not found in local collection for update.", farrow.FarrowingEventId);
+            }
+        }
+
+        private void FarrowingStoreOnFarrowingAdded( FarrowEvent farrow )
+        {
+            _farrowEvents.Add(farrow);
+        }
 
         #endregion
 
@@ -95,20 +139,13 @@ namespace PiggsCare.Core.ViewModels.Farrowing
             try
             {
                 _farrowEvents!.Clear();
-                await _farrowingStore.Load(BreedingEventId);
-                int loadedCount = _farrowingStore.FarrowEvents.Count();
-                Console.WriteLine($"Loaded {loadedCount} Farrowing events");
+                await _farrowingService.GetAllFarrowingsAsync(BreedingEventId);
 
-                foreach (FarrowEvent farrowEvent in _farrowingStore.FarrowEvents)
+                foreach (FarrowEvent farrowEvent in _farrowingStore.Farrowings)
                 {
                     _farrowEvents.Add(farrowEvent);
                 }
                 UpdateView();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
             }
             finally
             {
@@ -123,13 +160,13 @@ namespace PiggsCare.Core.ViewModels.Farrowing
 
         private void ExecuteOpenInsertRecordDialog()
         {
-            // Open the Farrow Create form dialog
+            // Open the Farrow AddAnimal form dialog
             _modalNavigationControl.PopUp<FarrowingCreateFormViewModel>(BreedingEventId);
         }
 
         private void ExecuteOpenModifyRecordDialog( int id )
         {
-            // Open the Farrow Modify form dialog
+            // Open the Farrow UpdateAnimal form dialog
             _modalNavigationControl.PopUp<FarrowingModifyFormViewModel>(id);
         }
 
@@ -139,7 +176,7 @@ namespace PiggsCare.Core.ViewModels.Farrowing
             _modalNavigationControl.PopUp<FarrowingDeleteFormViewModel>(id);
         }
 
-        // Opens the WeaningListingViewModel and passes in the breeding event id
+        // Opens the WeaningListingViewModel and passes in the insemination event id
         private void ExecuteOpenWeaningEventDialog( int id )
         {
             WeaningListingViewModel? viewmodel = _viewModelFactory.CreateViewModel<WeaningListingViewModel, int>(id);

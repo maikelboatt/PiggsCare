f4daedea8815fb4a95662742553686fe26cd0fb8
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-08-19 15:46:55 +0000

    Renamed breeding to insemination for clarity

--- PiggsCare.DataAccess/DTO/InseminationEventDto.cs
+++ PiggsCare.DataAccess/DTO/InseminationEventDto.cs
@@ -1,12 +1,11 @@
 namespace PiggsCare.DataAccess.DTO
 {
-    public class BreedingEventWithAnimalDto
+    public class InseminationEventDto
     {
         public int BreedingEventId { get; init; }
         public required int AnimalId { get; init; }
         public required DateTime AiDate { get; init; }
         public required DateTime ExpectedFarrowDate { get; init; } // Nullable DateTime
-        public required string Name { get; init; }
 
         public int? SynchronizationEventId { get; init; }
     }
--- PiggsCare.DataAccess/DTO/InseminationEventWithAnimalDto.cs
+++ PiggsCare.DataAccess/DTO/InseminationEventWithAnimalDto.cs
@@ -1,14 +1,13 @@
 namespace PiggsCare.DataAccess.DTO
 {
-    public class BreedingEventDto
+    public class InseminationEventWithAnimalDto
     {
         public int BreedingEventId { get; init; }
         public required int AnimalId { get; init; }
         public required DateTime AiDate { get; init; }
         public required DateTime ExpectedFarrowDate { get; init; } // Nullable DateTime
+        public required string? Name { get; init; }
 
         public int? SynchronizationEventId { get; init; }
     }
 }
-
-

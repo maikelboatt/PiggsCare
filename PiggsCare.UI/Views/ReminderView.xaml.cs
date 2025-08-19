032697f937e1496c861e5cae212dbcf17f811786
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-08-19 16:04:49 +0000

    A view that contains the ScheduleNotificationListing component to display all insemination reminders

---
+++ PiggsCare.UI/Views/ReminderView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace PiggsCare.UI.Views
+{
+    public partial class ReminderView:MvxWpfView
+    {
+        public ReminderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
